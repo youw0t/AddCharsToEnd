27ab367056bce65c7d069aa314a4109123f0ac08
youw0t <mrflashlf@gmail.com>
2017-02-09 19:42:58 +0100

    Added file creation

--- addCharToEnd.py
+++ addCharToEnd.py
@@ -2,7 +2,7 @@ import time
 
 def addCharToEnd(charToAdd, fileIn, fileOut):
     add = charToAdd
-    with open(fileOut, 'w') as out_file:
+    with open(fileOut, 'w+') as out_file:
         with open(fileIn, 'r') as in_file:
             for line in in_file:
                 out_file.write(line.rstrip('\n') + add)
@@ -12,7 +12,9 @@ while True:
     choice = input("Type INPUT or QUIT\n").upper()
     if(choice == 'INPUT'):
         inputfile = input("Enter input file: \n")
-        outputfile = input("Enter output file name: \n")
+        outputfile = (input("Enter output file name: \n"))
+        if '.' not in outputfile:
+            outputfile = outputfile + '.' + inputfile.rsplit('.', 1)[1]
         addCharToEnd(',', inputfile, outputfile)
         print('Done')
         time.sleep(3)
