d1ce5ce6d76ba4dff04d9b7fbee3ac50ff0a2b3e
youw0t <mrflashlf@gmail.com>
2017-02-09 17:16:28 +0100

    Added files

---
+++ README.md
@@ -0,0 +1,6 @@
+# AddCharsToEnd
+Quick program to add characters to lines in a file
+
+## Usage
+- Type the input file
+- Type the already created output file
---
+++ addCharToEnd.py
@@ -0,0 +1,25 @@
+import time
+
+def addCharToEnd(charToAdd, fileIn, fileOut):
+    add = charToAdd
+    with open(fileOut, 'w') as out_file:
+        with open(fileIn, 'r') as in_file:
+            for line in in_file:
+                out_file.write(line.rstrip('\n') + add)
+
+
+while True:
+    choice = input("Type INPUT or QUIT\n").upper()
+    if(choice == 'INPUT'):
+        inputfile = input("Enter input file: \n")
+        outputfile = input("Enter output file name: \n")
+        addCharToEnd(',', inputfile, outputfile)
+        print('Done')
+        time.sleep(3)
+        break
+    elif(choice == 'QUIT'):
+        break
+    else:
+        print("Not a command")
+        continue
+        
